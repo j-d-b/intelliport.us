2d4e56d61099cb574e619cc9815d6aa4ae319b48
j-d-b <jacob@jdbrady.info>
2018-01-19 01:24:27 -0500

    base layout, favicons

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  var hash = $(location).attr('hash');
+  if (hash == ""){ // index.html defaults to home
+    hash = "#home";
+  }
+  $('#content').html($(''+hash+'-content').html()); // load w/ correct content
+  $('.navbar-nav a[href="'+hash+'"]').addClass('nav-active'); // highlight current section
+
+  // "switch page" -- spa version
+  $('.nav-item').click(function() {
+    // change nav-active highlight
+    if ($(this).hasClass('nav-active')){
+      return;
+    }
+    else {
+      $('.nav-item').removeClass('nav-active');
+      $(this).addClass('nav-active');
+
+      // switch content
+      var ref = $(this).attr('href');
+      $('#content').html($(''+ref+'-content').html());
+    }
+  });
+
+});
