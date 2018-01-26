03d6352ad5c1397763db15143582c333f043711c
j-d-b <jacob@jdbrady.info>
2018-01-26 10:42:49 -0500

    abusing commit to share files to server

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
   $('#tests').on('click', function() {
     alert("test clicked");
 
-    $("#content").load( "http://ec2-54-197-199-71.compute-1.amazonaws.com/iport/_dsm.html", function(){
+    $("#content").load( "/_dsm.html", function(){
       alert("he worked");
     });
   });
